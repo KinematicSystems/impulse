fa62b741853792d94f1f592917e7d761501b32a9
Matt Grippaldi <mattg@kinematicsystems.com>
2015-03-08 14:13:36 -0400

    Numerous updates to support workspace

--- ui/src/common/impulse/impulse-service.js
+++ ui/src/common/impulse/impulse-service.js
@@ -13,86 +13,131 @@ angular.module('services.ImpulseService', [ 'dialogs.main' ]).factory('impulseSe
    	   Accepted = 'A'; // Invitee accepted invite
    	   Joined = 'J'; // Invitee is now a member of forum
    		Suspended = 'S'; // Forum membership suspended
+   		Left = 'L'; // User Left Forum
     */
    var enrollmentStatusMap = {
-      'J' : 'Joined',
-      'A' : 'Accepted',
-      'I' : 'Invited',
-      'R' : 'Rejected',
-      'P' : 'Pending',
-      'D' : 'Declined',
-      'S' : 'Suspended'
+      'J': 'Joined',
+      'A': 'Accepted',
+      'I': 'Invited',
+      'R': 'Rejected',
+      'P': 'Pending',
+      'D': 'Declined',
+      'S': 'Suspended',
+      'L': 'Left'
    };
+
+   var currentUser = null;
+   var collaborator = false;
 
    // Return the service object functions
    return {
-      enrollmentString : function(statusCode) {
+      enrollmentString: function(statusCode) {
          return enrollmentStatusMap[statusCode];
       },
-      showError : function(heading, msg) {
+      setCurrentUser: function(currUser) {
+         currentUser = currUser;
+      },
+      getCurrentUser: function() {
+         return currentUser;
+      },
+      setCollaborator: function(collab) {
+         collaborator = collab;
+      },
+      isCollaborator: function() {
+         return collaborator;
+      },
+      showError: function(heading, msg) {
          dialogs.error(heading, msg, {
-            size : 'sm'
+            size: 'sm'
          });
       },
-      showPrompt : function(heading, label, msg, defaultValue) {
+      showNotification: function(heading, msg) {
+         dialogs.notify(heading, msg, {
+            size: 'sm'
+         });
+      },
+      showPrompt: function(heading, label, msg, defaultValue) {
          var params = {
-            heading : heading,
-            label : label,
-            msg : msg,
-            defaultValue : defaultValue
+            heading: heading,
+            label: label,
+            msg: msg,
+            defaultValue: defaultValue
          };
          return dialogs.create('../common/impulse/prompt-dialog.html', 'promptDialogCtrl', params, {
-            size : 'sm'
+            size: 'sm'
          });
       },
-      showConfirm : function(heading, msg) {
+      showConfirm: function(heading, msg) {
          return dialogs.confirm(heading, msg, {
-            size : 'sm'
+            size: 'sm'
          });
       }
    };
 } ])
 
-.controller(
-      'promptDialogCtrl',
-      function($scope, $modalInstance, data) {
-         $scope.promptValue = data.defaultValue;
-         $scope.promptLabel = data.label;
-         $scope.promptMessage = data.msg;
-         $scope.promptHeading = data.heading;
+.controller('promptDialogCtrl', function($scope, $modalInstance, data) {
+   $scope.promptValue = data.defaultValue;
+   $scope.promptLabel = data.label;
+   $scope.promptMessage = data.msg;
+   $scope.promptHeading = data.heading;
 
-         $scope.cancel = function() {
-            $modalInstance.dismiss('Canceled');
-         }; // end cancel
+   $scope.cancel = function() {
+      $modalInstance.dismiss('Canceled');
+   }; // end cancel
 
-         $scope.save = function() {
-            $modalInstance.close($scope.promptValue);
-         }; // end save
+   $scope.save = function() {
+      $modalInstance.close($scope.promptValue);
+   }; // end save
 
-         $scope.hitEnter = function(evt) {
-            if (angular.equals(evt.keyCode, 13) && !(angular.equals($scope.user.name, null) || angular.equals(
-                  $scope.promptValue, '')))
-            {
-               $scope.save();
-            }
-         };
-      }) // end controller(promptDialogCtrl)
+   $scope.hitEnter = function(evt) {
+      if (angular.equals(evt.keyCode, 13) && !(angular.equals($scope.promptValue, null) || angular.equals($scope.promptValue, '')))
+      {
+         $scope.save();
+      }
+   };
+}) // end controller(promptDialogCtrl)
 
 .constant('LOGIN_EVENTS', {
-   LOGIN_SUCCESS : 'auth-login-success',
-   LOGIN_FAILED : 'auth-login-failed',
-   LOGOUT_SUCCESS : 'auth-logout-success',
-   SESSION_TIMEOUT : 'auth-session-timeout',
-   NOT_AUTHENTICATION : 'auth-not-authenticated',
-   NOT_AUTHORIZED : 'auth-not-authorized'
+   LOGIN_SUCCESS: 'auth-login-success',
+   LOGIN_FAILED: 'auth-login-failed',
+   LOGOUT_SUCCESS: 'auth-logout-success',
+   SESSION_TIMEOUT: 'auth-session-timeout',
+   NOT_AUTHENTICATION: 'auth-not-authenticated',
+   NOT_AUTHORIZED: 'auth-not-authorized'
+})
+
+.constant('COLLAB_EVENTS', {
+   SYNCHRONIZE: 'SYNCHRONIZE',
+   MESSAGE: 'MESSAGE',
+   EVENT: 'EVENT',
+   FORUM: {
+      ENROLLMENT: 'FORUM_ENROLLMENT',
+      CHANGE: 'FORUM_CHANGE',
+      NODE_CHANGE: 'FORUM_NODE_CHANGE',
+      CT_CREATE: 'CREATE', // CT = change type
+      CT_UPDATE: 'UPDATE',
+      CT_DELETE: 'DELETE',
+   },
+   USER: {
+      INVITE: 'USER_INVITE',
+      REMOVED: 'USER_REMOVED'
+   }
+})
+
+.constant('APP_EVENTS', {
+   MAP: {
+      GOTO_LOCATION: 'MAP_GOTO_LOCATION'
+   },
+   ACTIVATE_MODULE: 'ACTIVATE_MODULE'
 })
 
 .constant('ENROLLMENT_STATUS', {
-   JOINED : 'J',
-   ACCEPTED : 'A',
-   INVITED : 'I',
-   REJECTED : 'R',
-   PENDING : 'P',
-   DECLINED : 'D',
-   SUSPENDED : 'S'
+   JOINED: 'J',
+   ACCEPTED: 'A',
+   INVITED: 'I',
+   REJECTED: 'R',
+   PENDING: 'P',
+   DECLINED: 'D',
+   SUSPENDED: 'S',
+   LEFT: 'L'
 });
--- ui/src/common/login/login-service.js
+++ ui/src/common/login/login-service.js
@@ -1,69 +1,89 @@
 /**
- * This service makes the calls to the impulse login API.  It hooks the 401 errors from the service call
- * with the security interceptor added to $http.  The interceptor must be hooked in the config phase of the 
- * service module. (mattg)
+ * This service makes the calls to the impulse login API. It hooks the 401
+ * errors from the service call with the security interceptor added to $http.
+ * The interceptor must be hooked in the config phase of the service module.
+ * (mattg)
  */
 
 // loginService server API
-angular.module('services.LoginService',['services.ImpulseService']).factory('loginService', ['$http', '$window', '$rootScope','LOGIN_EVENTS',
-    function($http, $window, $rootScope, LOGIN_EVENTS) { 
-	var apiUrl = '/impulse/api/';
-	
-	// Return the service object functions
-	return {
-		currentUser: function() {
-			return $window.sessionStorage.getItem("userInfo");
-		},
-		restoreSession: function() {
-			// If the user is still in the session fire a login success
-			var storedUser = $window.sessionStorage.getItem("userInfo");
-			if (storedUser !== undefined && storedUser !== null && storedUser !== "null")
-			{
-		       	$rootScope.$broadcast(LOGIN_EVENTS.LOGIN_SUCCESS, storedUser);
-			}
-		},
-		login: function(userId, password) {
-			$http({method: 'POST', data: {userId: userId, password: password}, url: apiUrl + 'login'})
-			.success(function(data, status) {
-				if (status === 200) 
-				{	
-		           	$rootScope.$broadcast(LOGIN_EVENTS.LOGIN_SUCCESS, userId);
-			        $window.sessionStorage.setItem("userInfo", userId);
-				}
-				else
-				{
-			        $window.sessionStorage.removeItem("userInfo");
-		           	$rootScope.$broadcast(LOGIN_EVENTS.LOGIN_FAILED,"Bad Login!");
-				}
-			})
-			.error(function(data, status) {
-		        $window.sessionStorage.removeItem("userInfo");
-	           	$rootScope.$broadcast(LOGIN_EVENTS.LOGIN_FAILED,data.details); // Error details
-			});
-		},
-		logout: function() {
-			$http({method: 'GET',url: apiUrl + 'logout'})
-			.success(function(data, status) {
-		        $window.sessionStorage.removeItem("userInfo");
-	           	$rootScope.$broadcast(LOGIN_EVENTS.LOGOUT_SUCCESS);
-			});
-		} 
-	};
-}])
+angular.module('services.LoginService', [ 'services.ImpulseService' ]).factory(
+      'loginService',
+      [ '$http', '$window', '$rootScope', 'impulseService', 'LOGIN_EVENTS',
+            function($http, $window, $rootScope, impulseService, LOGIN_EVENTS) {
+               var apiUrl = '../api/';
+
+               // Return the service object functions
+               return {
+                  isLoggedIn: function() {
+                     var storedUser = $window.sessionStorage.getItem("userInfo");
+                     return (storedUser !== undefined && storedUser !== null && storedUser !== "null");
+                  },
+                  restoreSession: function() {
+                     // If the user is still in the session fire a login success
+                     var storedUserData = $window.sessionStorage.getItem("userInfo");
+                     if (storedUserData !== undefined && storedUserData !== null && storedUserData !== "null")
+                     {
+                        var storedUser = JSON.parse(storedUserData);
+                        impulseService.setCurrentUser(storedUser.userId);
+                        impulseService.setCollaborator((storedUser.accessLevel === 'sysuser'));
+                        $rootScope.$broadcast(LOGIN_EVENTS.LOGIN_SUCCESS, storedUser);
+                     }
+                  },
+                  login: function(userId, password) {
+                     $http({
+                        method: 'POST',
+                        data: {
+                           userId: userId,
+                           password: password
+                        },
+                        url: apiUrl + 'login'
+                     }).success(function(data, status) {
+                        if (status === 200)
+                        {
+                           var userInfo = {userId: userId, accessLevel: data};
+                           $window.sessionStorage.setItem("userInfo", JSON.stringify(userInfo));
+                           impulseService.setCurrentUser(userId);
+                           impulseService.setCollaborator((data === 'sysuser'));
+                           $rootScope.$broadcast(LOGIN_EVENTS.LOGIN_SUCCESS, userInfo);
+                        }
+                        else
+                        {
+                           $window.sessionStorage.removeItem("userInfo");
+                           impulseService.setCurrentUser(null);
+                           $rootScope.$broadcast(LOGIN_EVENTS.LOGIN_FAILED, "Bad Login!");
+                        }
+                     }).error(function(data, status) {
+                        $window.sessionStorage.removeItem("userInfo");
+                        impulseService.setCurrentUser(null);
+                        $rootScope.$broadcast(LOGIN_EVENTS.LOGIN_FAILED, data.details); // Error details
+                     });
+                  },
+                  logout: function() {
+                     $http({
+                        method: 'GET',
+                        url: apiUrl + 'logout'
+                     }).success(function(data, status) {
+                        $window.sessionStorage.removeItem("userInfo");
+                        impulseService.setCurrentUser(null);
+                        $rootScope.$broadcast(LOGIN_EVENTS.LOGOUT_SUCCESS);
+                     });
+                  }
+               };
+            } ])
 // securityInterceptor $http interceptor
-.factory('securityInterceptor', ['$q', '$rootScope', 'LOGIN_EVENTS', function($q, $rootScope, LOGIN_EVENTS) {
-        return {
-             'responseError': function (rejection) {
-               if (rejection.status === 401) 
-               {
-            	   var param = rejection.config;
-            	   $rootScope.$broadcast(LOGIN_EVENTS.NOT_AUTHENTICATED,"Not logged in!");
-               }
-               return $q.reject(rejection);
-            }
-        };
-}])
+.factory('securityInterceptor', [ '$q', '$rootScope', 'LOGIN_EVENTS', function($q, $rootScope, LOGIN_EVENTS) {
+   return {
+      'responseError': function(rejection) {
+         if (rejection.status === 401)
+         {
+            var param = rejection.config;
+            $rootScope.$broadcast(LOGIN_EVENTS.NOT_AUTHENTICATED, "Not logged in!");
+         }
+         return $q.reject(rejection);
+      }
+   };
+} ])
 // Add the sercurity intercept   or to the $http service in config phase
-.config(['$httpProvider', function($httpProvider) {
-    $httpProvider.interceptors.push('securityInterceptor');
-}]);
+.config([ '$httpProvider', function($httpProvider) {
+   $httpProvider.interceptors.push('securityInterceptor');
+} ]);
