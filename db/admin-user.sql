34c7bf73a0037499ffd4cb1bd301d3ec98625219
Matt Grippaldi <mattg@kinematicsystems.com>
2015-05-14 10:11:45 -0400

    Updated script to support encrypted password

--- db/admin-user.sql
+++ db/admin-user.sql
@@ -1,17 +1,8 @@
 -- Initial admin password is 'change.me'
-INSERT INTO `user_account`
-(`id`,
-`firstName`,
-`lastName`,
-`password`,
-`enabled`,
-`sysadmin`,
-`email`)
+REPLACE INTO `user_account` (`id`,`firstName`,`lastName`,`enabled`,`sysadmin`,`sysuser`,
+`initials`,`middleName`,`email`,`organization`,`phone`,`poc`,`pocPhone`,`notes`)
 VALUES
-("admin",
- "Admin",
- "SysAdmin",
- "$2y$10$c5OnMdM4h0zTp0WkwbnLMuMLftM9IKGqcVjkgUzuz.KQznQ89S8pu",
- 1,
- 1,
- "admin@yourserver.com");
+('admin','Admin','Admin',1,1,0,'SA','Admin','admin@yourserver.co','Systems Administration','999-555-1212','Sys Admin','999-111-2222',NULL);
+
+REPLACE INTO `user_credentials` (`id`,`password`) VALUES
+('admin','$2y$10$c5OnMdM4h0zTp0WkwbnLMuMLftM9IKGqcVjkgUzuz.KQznQ89S8pu');
